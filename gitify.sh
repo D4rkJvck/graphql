f99b5c947e862e9ef9197cc7b910def5a4b3e8e9
D4rkJvck <d4rkjvck@gmail.com>
2024-07-24 12:24:41 +0000

    Finalizing...

--- gitify.sh
+++ gitify.sh
@@ -75,7 +75,7 @@ commit_and_push() {
     read -r -p "Enter Commit Message: " commit_message
     git commit -a -m "$commit_message"
     git push origin "$desired_branch"
-    git push github "$desired_branch"
+    git push mirror "$desired_branch"
 
     log "Well done!"
 }
